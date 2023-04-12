4c559b8fc65eefeee7d56ca07f6a3269bb43cde5
Xavi Guasch <xaviguasch@gmail.com>
2023-04-12 20:03:22 +0200

    solved 2nd exercise: Keeping track of guesses

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,9 +1,9 @@
 import React from 'react'
-
 import { sample } from '../../utils'
 import { WORDS } from '../../data'
 
-import GuessInput from '../GuessInput/GuessInput'
+import GuessInput from '../GuessInput'
+import GuessResults from '../GuessResults'
 
 // Pick a random word on every pageload.
 const answer = sample(WORDS)
@@ -11,11 +11,18 @@ const answer = sample(WORDS)
 console.info({ answer })
 
 function Game() {
+  const [guesses, setGuesses] = React.useState([])
+
+  function handleSubmitGuess(tentativeGuess) {
+    setGuesses([...guesses, tentativeGuess])
+  }
+
   return (
     <>
       <GuessInput />
+      <GuessResults guesses={guesses} />
+      <GuessInput handleSubmitGuess={handleSubmitGuess} />
     </>
   )
 }
-
 export default Game
